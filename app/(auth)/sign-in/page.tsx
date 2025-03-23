f889a18b2950a8da130cd31a2d81649a4db523e7
Lei Zhi Guang <rayguang123@gmail.com>
2025-03-23 23:53:05 +0800

    Implement the UI of the Question Form

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 
 import AuthForm from "@/components/forms/AuthForm";
-import { SignInSchema } from "@/lib/validation";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
   return (
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 
 import AuthForm from "@/components/forms/AuthForm";
-import { SignUpSchema } from "@/lib/validation";
+import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
   return (
